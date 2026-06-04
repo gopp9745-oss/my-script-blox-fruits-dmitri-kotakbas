dd812791b6925302b952bccfe6755aaca149d8fd
PvZ Player <pvzonline@game.com>
2026-06-04 23:38:36 +0700

    Fix branch from main to master in loader

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/gopp9745-oss/my-script-blox-fruits-dmitri-kotakbas/main/BloxFruits.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gopp9745-oss/my-script-blox-fruits-dmitri-kotakbas/master/BloxFruits.lua"))()
